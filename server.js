8ad3904ae0939323a0ec0718ae1848bb69f88b2c
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2012-12-05 00:31:13 -0400

    add specs from spec folder

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
-//set path to the views (template) directory
-app.set('views', __dirname + '/views');
-app.use(express.static('public'));
-app.get('/', function(req, res){res.render('index.jade', {title: 'Jasmine-Server'});});
+var specs = [];
+fs.readdir('./spec/', function(err, files) {
+  files.forEach( function (file) {
+    specs.push('/spec/'+file);
+  });
+  //set path to the views (template) directory
+  app.set('views', __dirname + '/views');
+  app.get('/', function(req, res){res.render('index.jade', {specs: specs});});
+  
+  app.use('/jasmine-server', express.static(__dirname + '/public'));
+  app.use('/spec', express.static('./spec'));
+  app.use(express.static('public'))
 
-app.listen(80);
-console.log('Listening on port 80');
+  app.listen(80);
+  console.log('Listening on port 80');
+});
+
