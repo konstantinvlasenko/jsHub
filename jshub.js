4051bbad3a79102b8310956dacbf823013a5b746
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2012-12-09 00:40:57 -0500

    refactor

--- jshub.js
+++ jshub.js
@@ -1,36 +1,9 @@
-var server = require('./lib/server')
+var server = require('./lib/server');
+var runner = require('./lib/runner')
 var fs = require('fs');
 var async = require('async');
-var tap = require('tap');
 var _ = require('underscore');
 var glob = require('glob');
-var runner = require('./runners/saucelabs.js');
-
-var runTests = function(config, callback) {
-  var _tapProducer = new tap.Producer(true);
-  _tapProducer.pipe(process.stdout);
-  var _results = [];
-    
-  var _queue = async.queue(function (b, callback) {
-    runner.run(config, b, callback); 
-  }, config.concurrency);
-  _queue.drain = function() {
-    _.each(_results, function(r) { _tapProducer.write(r); } );
-    _tapProducer.end();
-    callback(null);
-  };
-  
-  _.each(config.browsers, function(b, index) { 
-    _.extend(b, {name: config.name + ' ( ' + b.browserName + ':' + b.version + ' on ' + b.platform  + ' )'});
-    _queue.push( b, function(passed) { 
-      _results.push({
-        id: index,
-        ok: passed,
-        name: b.name
-      });
-    });
-  });
-}
 
 var resolveServeFiles = function(config, callback) {
   async.reduce(config.serve_files, [], function(curr, pattern, next){
@@ -57,7 +30,7 @@ async.waterfall([
   getConfig,
   resolveServeFiles,
   server,
-  runTests
+  runner
   ], function (err, result) {
     process.exit();
   }
