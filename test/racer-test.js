6484baf8007d37cc6a8108e5063cd52cc57dba03
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2012-12-09 12:32:45 -0500

    more tests

--- test/racer-test.js
+++ test/racer-test.js
@@ -1,9 +1,11 @@
 var assert = require("assert");
 var racer = require('../lib/racer.js');
+var _ = require('underscore');
 
 describe('racer', function(){
   var config;
   var options;
+  
   beforeEach(function(){
     options = [];
     config = {
@@ -26,6 +28,21 @@ describe('racer', function(){
       assert.equal("jsHub ( internet explorer:8 on Windows 2003 )", options[0].name);
       assert.equal("jsHub ( internet explorer:9 on Windows 2008 )", options[1].name);
       assert.equal("jsHub ( internet explorer:10 on Windows 2012 )", options[2].name);
+      done();
+    });
+  })
+  
+  it('default concurrency is 2', function(done){
+    racer(run, config, function() {
+      assert.equal(2, config.concurrency);
+      done();
+    });
+  })
+  
+  it('should use concurrency if specified', function(done){
+    _.extend(config, { concurrency : 5 } );
+    racer(run, config, function() {
+      assert.equal(5, config.concurrency);
       done();
     });
   })
